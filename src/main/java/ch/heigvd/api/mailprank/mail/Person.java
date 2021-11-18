c8af08eb4fcfa753ec3afa3b57672bd1535196ce
Melissa Gehring <melissagehringstx@gmail.com>
2021-11-18 11:20:53 +0100

    Implement Person

--- src/main/java/ch/heigvd/api/mailprank/mail/Person.java
+++ src/main/java/ch/heigvd/api/mailprank/mail/Person.java
@@ -1,4 +1,13 @@
 package ch.heigvd.api.mailprank.mail;
 
 public class Person {
+    private final String mail;
+
+    public Person(String mail){
+        this.mail = mail;
+    }
+
+    public String getMail() {
+        return mail;
+    }
 }
