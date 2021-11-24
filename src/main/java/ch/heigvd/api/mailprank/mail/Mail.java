a82e8ae520c2b7e527061ef412580d617c44846d
Maelle <maelle.vogel@heig-vd.ch>
2021-11-24 17:39:44 +0100

    add base

--- src/main/java/ch/heigvd/api/mailprank/mail/Mail.java
+++ src/main/java/ch/heigvd/api/mailprank/mail/Mail.java
@@ -1,4 +1,20 @@
 package ch.heigvd.api.mailprank.mail;
 
 public class Mail {
+
+    public String getFrom(){
+        return  "";
+    }
+
+    public String[] getTo(){
+        return new String[]{"",""};
+    }
+
+    public String[] getCc(){
+        return new String[]{"",""};
+    }
+
+    public String getBody(){
+        return "";
+    }
 }
